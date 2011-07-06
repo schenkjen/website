19e882c6e425873f95b1f65c7a8de3e344fcfb57
unknown <Eric@.(none)>
2011-07-06 05:32:24 -0500

    first files for django set up

---
+++ settings.py
@@ -0,0 +1,178 @@
+# Django settings for myproject project.
+import os
+DEVLOPMENT_MODE = True
+MAINTENANCE_MODE = False
+
+MYSQLMODE = False
+PGSQLMODE = True
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+                               
+if DEVLOPMENT_MODE:
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+    TEMPLATE_STRING_IF_INVALID = 'INVALID VARIABLE'
+    CACHE_BACKEND ='locmem:///' 
+    CACHE_TIMEOUT = 60*5
+    CACHE_PREFIX="Z"
+    SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+else:
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+    TEMPLATE_STRING_IF_INVALID = ''
+    SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+    CACHE_BACKEND ='locmem:///' 
+    CACHE_TIMEOUT = 60*5
+    CACHE_PREFIX="Z"
+
+    
+ADMINS = (
+     ('Eric Satterwhite', 'webmaster@muskegohitmen.com'),
+)
+MANAGERS = (
+    ('Samantha Radloff', 'samantha@samanthajoyphotography.com'),
+)
+EMAIL_HOST = 'smtp.webfaction.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'sjphoto'
+EMAIL_HOST_PASSWORD = 'da93bfac'
+DEFAULT_FROM_EMAIL = 'noreply@samanthajoyphotography.com'
+SERVER_EMAIL = 'noreply@samanthajoyphotohraphy.com'
+
+if MYSQLMODE:
+    DATABASE_ENGINE = 'mysql'
+    DATABASE_USER = 'erics'
+    DATABASE_PASSWORD = 'qxm8uavi'
+
+if PGSQLMODE:
+    DATABASES = {
+        'default':{
+            "ENGINE":'django.db.backends.postgresql_psycopg2',
+            "NAME":"sjphoto",
+            "USER":'postgres',
+            "PASSWORD":'qxm8uavi'
+        }
+    }
+
+
+TIME_ZONE = 'America/Chicago'
+INTERNAL_IPS = ('127.0.0.1',)
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = False
+ACCOUNT_ACTIVATION_DAYS = 7
+FORCE_LOWERCASE_TAGS = True
+COMMENTS_HIDE_REMOVED = True
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+STATIC_DOC_ROOT = 'media/'
+MEDIA_ROOT = PROJECT_ROOT + '/media/'
+#MEDIA_ROOT = '/home/sjphoto/webapps/nginx/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = 'http://127.0.0.1:8000/static_media/'
+#MEDIA_URL = 'http://media.samanthajoyphotography.com/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'dn4d@jqg^+%&ww79dihu*0f^s#f!96^*8vyk4ih-=1nhc-+8_k'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.eggs.load_template_source',
+)
+THUMBNAIL_PROCESSORS = (
+    # Default processors - sorl thumbnail
+    'sorl.thumbnail.processors.colorspace',
+    'sorl.thumbnail.processors.autocrop',
+    'sorl.thumbnail.processors.scale_and_crop',
+    'sorl.thumbnail.processors.filters',
+    )
+
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'pagination.middleware.PaginationMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    #'django.middleware.transaction.TransactionMiddleware',
+    'maintenancemode.middleware.MaintenanceModeMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+    "django.middleware.doc.XViewMiddleware",
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+
+)
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug', # comment out when in production
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+)
+ROOT_URLCONF = 'sjphoto.urls'
+DJAPIAN_DATABASE_PATH = 'djapian_spaces/'
+DJAPIAN_STEMMING_LANG = "en"
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    PROJECT_ROOT +'/templates',
+    '/home/webapps/sjphoto_wsgi/lib/python2.5/debug_toolbar/templates',
+)
+#AUTHENTICATION_BACKENDS = (
+#   'satchmo_store.accounts.email-auth.EmailBackend',
+#   'django.contrib.auth.backends.ModelBackend',  
+#)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.comments',
+    'django.contrib.sitemaps',
+    'django.contrib.flatpages',
+    'django_extensions',
+    'template_utils',
+    'debug_toolbar',
+    'contact_form',
+    'pagination',
+    'hotsauce',
+    'debug_toolbar',
+    'overexposure',
+    'maintenancemode',
+    'south',
+    'tagging',
+    'djapian',
+    'registration',
+    'sorl.thumbnail',
+    'imagekit',
+    'messages',
+    'sjphoto.base',
+    'likeables',
+)
---
+++ urls.py
@@ -0,0 +1,76 @@
+from django.conf.urls.defaults import *
+#from satchmo_store.urls import urlpatterns
+from django.conf import settings
+from django.contrib import admin
+from djapian import load_indexes
+from overexposure.feeds import  BlogFeed
+
+
+load_indexes()
+admin.autodiscover()
+feeds={
+    'blogs': BlogFeed,       
+}
+urlpatterns = patterns('',
+    # Example:
+    # (r'^sjphoto/', include('sjphoto.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'sjphoto.base.views.basic.home_page', name="sjphoto_homepage"),
+    url(r'^gallery/(?P<slug>[-\w]+)/(?P<obj_id>\d+)/$', 'sjphoto.base.views.basic.home_page', name="sjphoto_feature_gallery_detail"),
+    url(r'^login/$', 'sjphoto.base.views.basic.login_user', name="sjphoto_login_user"),
+    url(r'^ajax/login/$', 'sjphoto.base.views.ajax.ajax_login', name="sjphoto_ajax_login_user"),
+    url(r'^logout/$', 'sjphoto.base.views.basic.logout_user', name="sjphoto_logout_user"),
+    url(r'^mail/multicompose/$', 'sjphoto.base.views.basic.multi_compose', name="sjphoto_multi_compose"),
+    url(r'^search/users/$', 'sjphoto.base.views.basic.user_search', name="sjphoto_user_search"),
+    url(r'^ajax/exif/(?P<photo_id>\d+)/$', 'sjphoto.base.views.ajax.get_exif', name="sjphoto_ajax_exif_url"),
+    url(r'^upload/photo/$', 'sjphoto.base.views.ajax.photo_upload', name="sjphoto_photo_test"),
+    url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}, name="overexposure_blog_feed"),
+    url(r'^search/photos/$', 'sjphoto.base.views.search.photo_search', name="sjphoto_photo_search"),
+    url(r'^search/$', 'sjphoto.base.views.search.ajax_main_search', name='sjphoto_main_search'),
+)
+
+urlpatterns += patterns('',
+   # url(r'^clients/', include('sjphoto.clientmanager.urls')),
+     (r'^static_media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_DOC_ROOT,'show_indexes': True}),                        
+)
+urlpatterns += patterns('sjphoto.base.views.basic',
+   url(r'^gallery/(?P<slug>[-\w]+)/$', 'gallery_view', name='pl-gallery'),                        
+)
+urlpatterns += patterns('',
+    url(r'^blog/', include('overexposure.urls')),
+)
+urlpatterns += patterns('',
+    url(r'^mail/', include('messages.urls')),
+)
+urlpatterns += patterns('',
+#    url(r'^albums/', include('photologue.urls')),
+)
+urlpatterns += patterns('',
+    url(r'^users/', include('registration.urls')),                        
+)
+urlpatterns += patterns('sjphoto.base.views.contests',
+   url(r'^contest/(?P<slug>[-\w]+)/(?P<ct_id>\d+)-(?P<obj_id>\d+)/$', 'photo_contest_detail', name='sjphoto_contest_detail'),                        
+)
+urlpatterns += patterns('',
+    url(r'^comments/', include('django.contrib.comments.urls')),
+)
+urlpatterns += patterns('',
+    url(r'^contact/', include('contact_form.urls')),
+)
+urlpatterns += patterns('sjphoto.base.views.gallery',
+    url(r'^albums/$', 'gallery_list', name='sjphoto_gallery_list'),
+    url(r'albums/(?P<slug>[-\w]+)/(?P<obj_id>\d+)/$', 'gallery_detail', name="sjphoto_gallery_detail"),
+)
+urlpatterns += patterns('',
+    url(r'likes/', include('likeables.urls'))                        
+)
+urlpatterns += patterns('',
+   url(r'^generic_photo/', include('photologue.urls')),
+)
